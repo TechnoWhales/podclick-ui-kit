9c4ffa9419f0bc211e579e90b8307da57e47b93d
Elena <el-ki-ri@yandex.ru>
2025-11-25 13:05:55 +0400

    add Button

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,125 @@
+import type { Meta, StoryObj, StoryFn, Args, StoryContext } from '@storybook/react-vite'
+import { action } from 'storybook/actions';
+
+import { Button } from '@/components/Button/Button'
+import { Icon } from '@/components/Icon/Icon'
+
+const ContainerDecorator = (Story: StoryFn<Args>, context: StoryContext<Args>) => {
+  const isFullwidth = context.args?.fullwidth;
+
+  return (
+    <div
+      style={{
+        width: '500px',
+        padding: '16px',
+        border: isFullwidth ? '1px dashed #0257b8ff' : 'none',
+      }}
+    >
+      {isFullwidth && (
+        <p
+          style={{
+            marginBottom: '50px',
+            textAlign: 'center',
+            color: '#0257b8ff',
+            fontSize: '14px',
+          }}
+        >
+          Parent component
+        </p>
+      )}
+      <div style={{ width: isFullwidth ? '100%' : 'fit-content', margin: '0 auto' }}>
+        {Story(context.args, context)}
+      </div>
+    </div>
+  );
+};
+
+const meta = {
+  title: 'Components/Button',
+  component: Button,
+  decorators: [ContainerDecorator],
+  args: {
+    onClick: action('Button clicked!'),
+  },
+  argTypes: {
+    fullwidth: { control: 'boolean' },
+    disabled: { control: 'boolean' },
+    as: { table: { disable: true } },
+    className: { table: { disable: true } },
+    // ref: { table: { disable: true } },
+  }
+} satisfies Meta<typeof Button>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    variant: 'primary',
+    children: 'Primary button',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    variant: 'secondary',
+    children: 'Secondary button',
+  },
+}
+
+export const Outlined: Story = {
+  args: {
+    variant: 'outlined',
+    children: 'Outlined button',
+  },
+}
+
+export const Link: Story = {
+  args: {
+    children: 'Link Button',
+    variant: 'link',
+  },
+}
+
+export const AsLink: Story = {
+  args: {
+    as: 'a',
+    children: 'Link that looks like a button',
+    variant: 'primary',
+  },
+}
+
+export const WithIcon: Story = {
+  args: {
+    children: <Icon iconId={'google'} />,
+    variant: 'icon',
+  },
+}
+
+export const WithIconAndText: Story = {
+  args: {
+    children: (
+      <>
+        <Icon iconId={'bookmarkOutline'} />
+        Favorites
+      </>
+    ),
+    variant: 'link',
+  },
+}
+
+export const Fullwidth: Story = {
+  args: {
+    children: 'Fullwidth button',
+    fullwidth: true,
+    variant: 'primary',
+  },
+}
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    children: 'Disabled Button',
+    variant: 'primary',
+  },
+}
---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,98 @@
+import type {
+  ComponentPropsWithoutRef,
+  ElementType,
+  ReactNode,
+} from 'react'
+
+import clsx from 'clsx'
+import { Icon, type IconsId } from '@/components/Icon/'
+import s from '@/components/Button/Button.module.css'
+
+/**
+ * Кнопка с поддержкой различных вариантов оформления
+ * 
+ * @example
+ * ```tsx
+ * <Button variant="primary" onClick={handleClick}>
+ *   Нажми меня
+ * </Button>
+ * ```
+ * 
+ * @param as - HTML-тег или компонент для рендеринга
+ * @param variant - Вариант внешнего вида кнопки
+ * @param fullwidth - Занимает всю доступную ширину
+ * @param disabled - Неактивное состояние
+ * @param iconId - ID иконки для отображения
+ */
+
+/* ---------------- TYPES ---------------- */
+
+export type ButtonOwnProps<T extends ElementType = 'button'> = {
+  /** HTML-тег или компонент для рендеринга */
+  as?: T
+  /** Содержимое кнопки */
+  children: ReactNode
+  /** Вариант внешнего вида */
+  variant?: 'icon' | 'link' | 'outlined' | 'primary' | 'secondary'
+  /** Занимает всю доступную ширину */
+  fullwidth?: boolean
+  /** Дополнительные CSS классы */
+  className?: string
+  /** Неактивное состояние */
+  disabled?: boolean
+  /** Обработчик клика */
+  onClick?: () => void, 
+  /** ID иконки */
+  iconId?: IconsId
+}
+
+/** 
+ * Комбинация собственных пропсов + HTML-пропсов элемента `as`
+ * Omit предотвращает конфликт типов со Storybook и HTML-атрибутами.
+ */
+export type ButtonProps<T extends ElementType = 'button'> =
+  ButtonOwnProps<T> &
+  Omit<ComponentPropsWithoutRef<T>, keyof ButtonOwnProps<T>>
+
+/* ---------------- COMPONENT ---------------- */
+
+export const Button = <T extends ElementType = 'button'>(props: ButtonProps<T>) => {
+  const {
+    variant = 'primary',
+    fullwidth,
+    disabled = false,
+    className,
+    iconId,
+    children,
+    onClick,
+
+    as: Component = 'button',
+    ...rest
+  } = props
+  const styles = clsx(
+    s.button,
+    {
+      [s.fullwidth]: fullwidth,
+      [s.disabled]: disabled,
+    },
+    s[variant],
+    className
+  )
+
+  if (variant === 'icon' && iconId) {
+    return (
+      <Component>
+        <Icon iconId={iconId} />
+      </Component>
+    )
+  }
+
+  return (
+    <Component className={styles} disabled={disabled} onClick={onClick} {...rest}>
+      {iconId && <Icon iconId={iconId} />}
+      {children}
+    </Component>
+  )
+
+
+}
